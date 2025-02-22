e2b59236179e72295f7d3c62cd12a1631edf2747
RJM <Rmorr2001@gmail.com>
2025-02-21 19:29:33 -0600

    Ai is always AI

--- simulation/frontend/src/components/app.js
+++ simulation/frontend/src/components/app.js
@@ -9,7 +9,7 @@ import NodeConfiguration from "./SimConfig/NodeConfiguration.js";
 import Dashboard from "./Dashboard/Dashboard.js";
 import Portfolio from "./Pages/about.js";
 import TechnicalAbout from "./Pages/TechnicalAbout.js";
-import AIAssistantProvider from "./Gemini/AiAssistantProvider.js";
+import AIAssistantProvider from "./Gemini/AIAssistantProvider.js";
 import AppNavbar from './AppNavbar.js';
 import AINavbarButton from "./Gemini/AiINavbarButton.js";
 
