e4899e80398a26fee11e0c7c63c1da9aec48a640
Rmorr2001 <149847349+Rmorr2001@users.noreply.github.com>
2025-02-23 20:24:22 -0600

    Update settings.py

--- simulation/simulation_core/settings.py
+++ simulation/simulation_core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lyxo8^r^*zhwq#yh&3r0inxma$z*hpkjr4bwdkfhk^2(zp_t8-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Add to your existing ALLOWED_HOSTS
 ALLOWED_HOSTS = ["*"]  # For development only. In production, specify your Cloud Run domain
